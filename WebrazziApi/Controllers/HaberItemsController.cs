c144a267f39d9e810d4f144cead3ff92c36de75b
pasadagli <kaplan_911@hotmail.com>
2021-10-23 16:58:30 +0300

    20211023 16:58

--- WebrazziApi/Controllers/HaberItemsController.cs
+++ WebrazziApi/Controllers/HaberItemsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebrazziApi.Models;
 
+//try it, try it hard
 namespace WebrazziApi.Controllers
 {
     [Route("api/[controller]")]
