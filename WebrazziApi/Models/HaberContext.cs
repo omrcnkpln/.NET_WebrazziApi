52aec360e6c6e5dc8f2730e385a46f9ea8085323
pasadagli <kaplan_911@hotmail.com>
2021-10-23 16:46:19 +0300

    20211023 16:46

---
+++ WebrazziApi/Models/HaberContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebrazziApi.Models
+{
+    public class HaberContext : DbContext
+    {
+        public HaberContext(DbContextOptions<HaberContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<HaberItem> HaberItems { get; set; }
+    }
+}
